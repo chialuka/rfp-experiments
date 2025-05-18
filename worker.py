5c6d51b114952c47bcadcc0c72bfc7e08220dca5
Chukwualuka Chiama <alukachiama@gmail.com>
2025-05-18 15:00:35 -0100

    remove ssl requirement in redis connection string

--- worker.py
+++ worker.py
@@ -7,7 +7,7 @@ load_dotenv()
 listen = ["high", "default", "low"]
 
 redis_url = os.getenv("REDIS_URL")
-conn = redis.from_url(redis_url, ssl=True, ssl_cert_reqs=None)
+conn = redis.from_url(redis_url, ssl_cert_reqs=None)
 
 if __name__ == "__main__":
     queues = [Queue(name, connection=conn) for name in listen]
